83f2feed20c17bae74f26b2ba3126d377ab5c40b
Roman Bakakin <bakakin@technokratos.com>
2020-10-07 09:55:59 +0300

    Маршрут по тайным тропам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Main from "../main/main";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import MainScreen from "../main-screen/main-screen";
+import SignInScreen from "../sign-in-screen/sign-in-screen";
+import MyListScreen from "../my-list-screen/my-list-screen";
+import FilmScreen from "../film-screen/film-screen";
+import AddReviewScreen from "../add-review-screen/add-review-screen";
+import PlayerScreen from "../player-screen/player-screen";
 
 const App = (props) => {
   const {filmPromo} = props;
 
   return (
-    <Main filmPromo={filmPromo} />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <MainScreen filmPromo={filmPromo} />
+        </Route>
+        <Route path="/login" exact>
+          <SignInScreen />
+        </Route>
+        <Route path="/mylist" exact>
+          <MyListScreen />
+        </Route>
+        <Route path="/films/:id" exact>
+          <FilmScreen />
+        </Route>
+        <Route path="/films/:id/review" exact>
+          <AddReviewScreen />
+        </Route>
+        <Route path="/player/:id" exact>
+          <PlayerScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
